52c39c839095def322e9351a925353d872e214a0
i.tupikov <i.tupikov@reglab.ru>
2024-10-16 20:05:14 +0500

    add: Вывод доп. информации об алгоритме

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ template <typename ArchivatorChild>
 void CompressAndDecompress(const std::filesystem::path& input_file,
                            const std::filesystem::path& output_compress_file,
                            const std::filesystem::path& output_decompress_file,
-                           std::string&& postfix) {
+                           std::string&& postfix, uintmax_t input_file_size) {
     std::unique_ptr<Archivator> archivator =
         std::make_unique<ArchivatorChild>();
 
@@ -28,15 +28,29 @@ void CompressAndDecompress(const std::filesystem::path& input_file,
         AddPostfix(output_decompress_file, std::move(postfix));
 
     {
-        LOG_DURATION("Compress " + postfix + ":");
+        LOG_DURATION("\tCompress time:");
         archivator->compress(input_file, compressed_huffman_file);
     }
+    auto compressed_file_size =
+        std::filesystem::file_size(compressed_huffman_file);
+    std::cout << "\tCompressed file size: " << compressed_file_size
+              << std::endl;
+    double reduction = static_cast<double>(input_file_size) -
+                       static_cast<double>(compressed_file_size);
+    double compression_percentage =
+        (reduction / static_cast<double>(input_file_size)) * 100.0;
+
+    std::cout << "\tCompression percentage: " << compression_percentage << "%"
+              << std::endl;
 
     {
-        LOG_DURATION("Decompress " + postfix + ":");
+        LOG_DURATION("\tDecompress time:");
         archivator->decompress(compressed_huffman_file,
                                decompressed_huffman_file);
     }
+    std::cout << "\tDecompressed file size: "
+              << std::filesystem::file_size(decompressed_huffman_file)
+              << std::endl;
 }
 
 int main(int argc, char* argv[]) {
@@ -51,13 +65,21 @@ int main(int argc, char* argv[]) {
     std::filesystem::path output_compress_file = argv[2];
     std::filesystem::path output_decompress_file = argv[3];
 
+    auto input_file_size = std::filesystem::file_size(input_file);
+
+    std::cout << "Input file size: " << input_file_size << std::endl;
+
+    std::cout << "\nHuffman alghorithm: " << std::endl;
     CompressAndDecompress<HuffmanArchivator>(input_file, output_compress_file,
-                                             output_decompress_file, "huffman");
+                                             output_decompress_file, "huffman",
+                                             input_file_size);
 
+    std::cout << "\nRLE alghorithm: " << std::endl;
     CompressAndDecompress<RleArchivator>(input_file, output_compress_file,
-                                         output_decompress_file, "rle");
+                                         output_decompress_file, "rle",
+                                         input_file_size);
 
-    std::cout << "Proccess completed" << std::endl;
+    std::cout << "\nProccess completed" << std::endl;
 
     return 0;
 }
